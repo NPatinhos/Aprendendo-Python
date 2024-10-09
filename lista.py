87da73025b914449778dd215f653d3d6adff4678
NPatinhos <109314887+NPatinhos@users.noreply.github.com>
2024-10-09 15:46:35 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,12 @@
+lista = ['Maria', 'Helena', 'Luiz']
+i=0
+
+for nome in lista: 
+        print(i, lista[i])
+        i+=1
+
+ #   OU ENTAO
+#indices = range(len(lista))
+
+for indice in range(len(lista)):
+        print(indice, lista[indice])
