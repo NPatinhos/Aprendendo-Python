ec6eccb228001f411affd510c7a3e854ee311e73
NPatinhos <109314887+NPatinhos@users.noreply.github.com>
2024-10-28 11:56:27 -0300

    consegui a ativ

--- intermediario/ativ_dic.py
+++ intermediario/ativ_dic.py
@@ -16,38 +16,25 @@ perguntas = [
         'Resposta': '5',
     },
 ]
-
-# certas = 0
-# def checar_resposta(palpite, pergunta['Resposta']):
-#     if palpite == pergunta['Resposta']:
-#         certas+=1
-#         print('Acertou')
-#     else:
-#         print('Errou')
+certas = 0
 
 for pergunta in perguntas:
-    print('\n', 'Pergunta:', pergunta.get('Pergunta'))
-    for item in enumerate(pergunta['Opções']):
-        indice, opcao = item 
+    print('\n', 'Pergunta:', pergunta['Pergunta'])
+
+    for indice, opcao in enumerate(pergunta['Opções']):
         print(f'{indice}) {opcao} ')
+
     palpite = input('Escolha uma opção: ')
     try:
         int(palpite)
     except TypeError as error:
             print('Errado')
 
-    if 'Opções'[int(palpite)] == 'Resposta':
-        print('certo')
+    palpite_resposta = pergunta['Opções'][int(palpite)]
+    if palpite_resposta == pergunta['Resposta']:
+        print('Certo')
+        certas+=1
     else:
          print('Errado')
 
-
-
-# ESSE METODO É ANTIGO, TEM Q FAZER COM AS FUNÇOES DE DICIONARIO keys() e values()
-# for questao in perguntas:
-#     print('\n', questao['Pergunta'])
-#     for item in enumerate(questao['Opções']):
-#         indice, opcao = item 
-#         print(f'{indice}) {opcao} ')
-
-    
+print(f'\nVocê acertou {certas} de {len(perguntas)} perguntas!\n')
