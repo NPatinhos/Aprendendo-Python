3a8ab16d375b56eedd4c6e1c8d1215b883fb5e6a
NPatinhos <109314887+NPatinhos@users.noreply.github.com>
2024-10-29 13:08:07 -0300

    consegui

--- intermediario/ativ_set.py
+++ intermediario/ativ_set.py
@@ -12,33 +12,48 @@ Requisitos:
     Se não encontrar duplicados na lista, retorne -1
 """
 lista_de_listas_de_inteiros = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
-    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
-    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
-    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
-    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
-    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
-    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
-    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
-    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
-    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],    # 0
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],     # 1
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],     # 2
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],     # 3
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],    # 4
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],     # 5
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],  # 6
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],     # 7
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],    # 8 
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],     # 9 
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],     # 10
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],    # 11
 ]
+def lista_set(lista):
+    lista_setada = []
+    for numero in set(lista):
+        lista_setada.append(numero)
+    return lista_setada
+        
+def ordena_lista(lista):
+    lista_ordenada = sorted(lista)
+    return lista_ordenada
 
-for lista in lista_de_listas_de_inteiros:
-    print('lista:')
+def conta_idx(lista):
+    num_repetido = 10
+    idx_repetido = 10
+    for i, num in enumerate(lista):
+        idx = 0 
+        for posic, j in enumerate(lista[i+1:]):
+            idx+=1
+            if num == j:
+                #print(num, idx)
+                if (posic + i + 1) < idx_repetido:
+                    num_repetido = num
+                    idx_repetido = posic+i+1
+                break
+    print(num_repetido)
 
-    print(set(lista))
+for posicao, lista in enumerate(lista_de_listas_de_inteiros):
+    print(f'lista {posicao}:')
 
-
-
-    # setar = [set(lista)]
-    # if setar == lista:
-    #     print('-1')
-    # else:
-    #     print('1')
-        # for item in lista:
-        #     if item == lista[item]:
-        #         duplicado = item
-        #     menor_indice = lista[item]
+    if ordena_lista(lista) == lista_set(lista):
+        print('-1')
+    else:
+        conta_idx(lista)
