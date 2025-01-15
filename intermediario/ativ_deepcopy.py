4c83af6425127c4e189a7600561dee8f3eb5fa5c
mdcarvalho12.fd@gmail.com <109314887+NPatinhos@users.noreply.github.com>
2025-01-15 17:19:43 -0300

    fiz

--- intermediario/ativ_deepcopy.py
+++ intermediario/ativ_deepcopy.py
@@ -1,4 +1,5 @@
 # copy, sorted, produtos.sort
+import copy
 # Exercícios
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
@@ -12,17 +13,28 @@ produtos = [
 
 # 1 Aumente os preços dos produtos a seguir em 10%
 
-for produto in produtos:
-    produto['preco'] *= 1.1
-print (produtos)
+#for produto in produtos:
+ #   produto['preco'] *= 1.1
 
 # 2 Gere novos_produtos por deep copy (cópia profunda)
+#novos_produtos = copy.deepcopy(produtos)
+
+#faz um deepcopy do original e modifica so o conteudo da copia
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.1 }
+    for produto in copy.deepcopy(produtos)
+]
 
 # 3 Ordene os produtos por nome decrescente (do maior para menor)
-
+novos_produtos = sorted(novos_produtos, key=lambda k : k['nome'], reverse=True)
 
 # 4 Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
-
+produtos_ordenador_por_nomes = copy.deepcopy(novos_produtos)
 # 5 Ordene os produtos por preco crescente (do menor para maior)
-
+novos_produtos = sorted(novos_produtos, key=lambda k : k['preco'])
 # 6 Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+produtos_ordenados_por_preco = copy.deepcopy(novos_produtos)
+
+print(*produtos, sep='\n')
+print()
+print(*produtos_ordenados_por_preco, sep='\n')
