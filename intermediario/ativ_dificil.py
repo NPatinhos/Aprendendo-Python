4fc16e771fae44f55ba0f7d8ef454169fb2937c5
mdcarvalho12.fd@gmail.com <109314887+NPatinhos@users.noreply.github.com>
2025-01-15 15:29:45 -0300

    Create ativ_dificil.py

---
+++ intermediario/ativ_dificil.py
@@ -0,0 +1,28 @@
+# copy, sorted, produtos.sort
+# Exercícios
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+
+
+# 1 Aumente os preços dos produtos a seguir em 10%
+
+for produto in produtos:
+    produto['preco'] *= 1.1
+print (produtos)
+
+# 2 Gere novos_produtos por deep copy (cópia profunda)
+
+# 3 Ordene os produtos por nome decrescente (do maior para menor)
+
+
+# 4 Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# 5 Ordene os produtos por preco crescente (do menor para maior)
+
+# 6 Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
